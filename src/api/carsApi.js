d1319ffcf914ea5cc5e4aea171b96ffb40592e83
Alamdar Bashirov <alamdarzb-sp202@code.edu.az>
2026-07-25 15:28:17 +0400

    added: getCar function at carsApi.js

--- src/api/carsApi.js
+++ src/api/carsApi.js
@@ -1,18 +1,27 @@
 import carsData from "../data/cars.json";
 import delay from '../utils/delay';
 
+let cars = [...carsData]
+
 export const getCars = async (query = {}) => {
   await delay(800)
-  return new Promise((resolve, reject) => {
-    const shouldFail = Math.random() < 0.2; // 20% error
-    if (shouldFail) {
-      reject(new Error("Failed to fetch cars"))
-      return;
-    }
-    let cars = [...carsData]
+  const shouldFail = Math.random() < 0.2;
 
-    resolve(cars)
-  });
+  if (shouldFail) {
+    throw new Error("Failed to fetch cars");
+  }
+
+  return cars;
 };
 
 
+export const getCar = async (id) => {
+  await delay(800)
+  const car = cars.find(car => car.id == Number(id))
+
+  if (!car) {
+    throw new Error ("Car not found")
+  }
+
+  return car;
+}
