fcdd3713a6e588c9743c47b4f457c4ce34f69920
Alamdar Bashirov <alamdarzb-sp202@code.edu.az>
2026-07-27 16:11:37 +0400

    added: driver name and email validation at booking page

--- src/app/booking/BookingPage.jsx
+++ src/app/booking/BookingPage.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './BookingPage.module.scss'
+import { useParams } from 'react-router-dom'
 
 const BookingPage = () => {
 
-    const [error, setError] = useState("")
+    const {id} = useParams()
+
+    const [validationError, setValidationError] = useState("")
     const [bookingData, setBookingData] = useState({
         startDate: "",
-        endDate: ""
+        endDate: "",
+        name: "",
+        email: ""
     })
 
     const [step, setStep] = useState(1)
@@ -18,62 +23,126 @@ const BookingPage = () => {
     const end = new Date(bookingData.endDate)
 
     const minRentDay = 2
-    const rentDay = (end - start) /  (1000 * 60 * 60 * 24)
+    const rentDay = (end - start) / (1000 * 60 * 60 * 24)
 
     const handleContinue = () => {
-        setError("")
+        setValidationError("")
+        //for step one
         if (start < today) {
-            setError("The date before today cannot be selected")
+            setValidationError("The date before today cannot be selected")
             return;
         }
         if (start >= end) {
-            setError("the end date must be at least 1 day after the start date")
+            setValidationError(`the end date must be at least ${minRentDay} day after the start date`)
             return;
         }
         if (rentDay < minRentDay) {
-            setError(`You must rent for a minimum of ${minRentDay} days.`)
+            setValidationError(`You must rent for a minimum of ${minRentDay} days.`)
             return;
         }
+        setStep(step + 1)
+    }
+
+    const handleDriverContinue = () => {
+        setValidationError("")
+        //for step two
+        if(!bookingData.name){
+            setValidationError("The name cannot be empty.")
+            return;
+        }
+        if(!bookingData.email){
+            setValidationError("The email cannot be empty.")
+            return;
+        }
+        if(!bookingData.email.includes("@") || !bookingData.email.includes(".")){
+            setValidationError("The email is not in the correct format.")
+            return;
+        }
+
         setStep(step + 1)
     }
 
     return (
         <div>
             <div>
-                <div>
-                    <div>
-                        <label htmlFor="startDate">Start Date</label>
-                        <input
-                            type="date"
-                            id='startDate'
-                            value={bookingData.startDate}
-                            min={formatedToday}
-                            onChange={(e) => setBookingData({
-                                ...bookingData,
-                                startDate: e.target.value
-                            })}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="endDate">End Date</label>
-                        <input
-                            type="date"
-                            id='endDate'
-                            value={bookingData.endDate}
-                            onChange={(e) => setBookingData({
-                                ...bookingData,
-                                endDate: e.target.value
-                            })}
-                        />
-                    </div>
+                {
+                    step === 1 && (
+                        <div>
+                            <div>
+                                <label htmlFor="startDate">Start Date</label>
+                                <input
+                                    type="date"
+                                    id='startDate'
+                                    value={bookingData.startDate}
+                                    min={formatedToday}
+                                    onChange={(e) => setBookingData({
+                                        ...bookingData,
+                                        startDate: e.target.value
+                                    })}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="endDate">End Date</label>
+                                <input
+                                    type="date"
+                                    id='endDate'
+                                    value={bookingData.endDate}
+                                    onChange={(e) => setBookingData({
+                                        ...bookingData,
+                                        endDate: e.target.value
+                                    })}
+                                />
+                            </div>
 
-                    <button
-                        disabled={!bookingData.startDate || !bookingData.endDate}
-                        onClick={handleContinue}
-                    >
-                        Continue
-                    </button>
-                </div>
+                            <button
+                                disabled={!bookingData.startDate || !bookingData.endDate}
+                                onClick={handleContinue}
+                            >
+                                Continue
+                            </button>
+                            {validationError && <span>{validationError}</span>}
+                        </div>
+                    )
+                }
+                {
+                    step === 2 && (
+                        <div>
+                            <div>
+                                <label htmlFor="driverName">Driver Name</label>
+                                <input
+                                    type="text"
+                                    id='driverName'
+                                    value={bookingData.name}
+                                    onChange={(e) => setBookingData({
+                                        ...bookingData,
+                                        name: e.target.value
+                                    })}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="driverEmail">Driver Email</label>
+                                <input
+                                    type="email"
+                                    id='driverEmail'
+                                    value={bookingData.email}
+                                    onChange={(e) => setBookingData({
+                                        ...bookingData,
+                                        email: e.target.value
+                                    })}
+                                />
+                            </div>
+
+                            <button
+                                disabled={!bookingData.name || !bookingData.email}
+                                onClick={handleDriverContinue}
+                            >
+                                Continue
+                            </button>
+
+                            {validationError && <span>{validationError}</span>}
+                        </div>
+                    )
+                }
             </div>
         </div>
     )
